ab2d55ab808051498bebdf227d430a21fb0a2dfe
Ana <anadecanha@gmail.com>
2024-12-17 12:17:07 +0000

    optimise navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,39 +6,33 @@ import Link from "next/link";
 import { Button } from "@headlessui/react";
 import ContactModal from "./ContactModal";
 
-const Navbar: React.FC = () => {
+// Memoize Navbar to prevent unnecessary re-renders
+const Navbar: React.FC = React.memo(() => {
   const [isModalOpen, setModalOpen] = useState(false);
-
-  const handleContactClick = () => {
-    setModalOpen(true);
-  };
 
   return (
     <nav className="fixed top-0 left-0 w-full z-50 bg-softWhite shadow-md rounded-b-3xl">
       <div className="flex justify-between items-center w-full px-4 md:px-16 h-12 mx-auto">
+        {/* Combined Logo */}
         <div className="flex items-center">
-          <div className="block md:hidden px-4">
-            {/* Mobile Logo */}
-            <Image
-              src="/s-icon.png"
-              priority={true}
-              alt="Mobile Logo"
-              width={15}
-              height={15}
-            />
-          </div>
-          <div className="hidden md:block">
-            {/* Desktop Logo */}
-            <Image
-              src="/logo-small.png"
-              priority={true}
-              alt="Logo"
-              width={150}
-              height={100}
-            />
-          </div>
+          <Image
+            src="/logo-small.png"
+            alt="Logo"
+            width={150}
+            height={100}
+            priority={true} // Only prioritize desktop logo
+            className="hidden md:block"
+          />
+          <Image
+            src="/s-icon.png"
+            alt="Mobile Logo"
+            width={15}
+            height={15}
+            className="block md:hidden"
+          />
         </div>
 
+        {/* Navigation Links */}
         <div className="flex space-x-4 font-poppins">
           <Link
             href="#services"
@@ -48,15 +42,17 @@ const Navbar: React.FC = () => {
           </Link>
           <Button
             className="text-deepPurple font-semiBold text-buttonText py-2 px-4"
-            onClick={handleContactClick}
+            onClick={() => setModalOpen(true)} // Inline handler for simplicity
           >
             Contact
           </Button>
         </div>
       </div>
+
+      {/* Contact Modal */}
       <ContactModal isOpen={isModalOpen} onClose={() => setModalOpen(false)} />
     </nav>
   );
-};
+});
 
 export default Navbar;
